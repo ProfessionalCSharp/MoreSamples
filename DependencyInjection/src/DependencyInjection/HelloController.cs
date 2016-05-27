5de2483ec359c7998d2fa370dc535e2be7ebf522
Christian Nagel <christian@christiannagel.com>
2016-05-27 15:08:10 +0200

    add dependency injection sample

---
+++ DependencyInjection/src/DependencyInjection/HelloController.cs
@@ -0,0 +1,15 @@
+﻿namespace DependencyInjection
+{
+    public class HelloController
+    {
+        private readonly IGreetingService _greetingService;
+        public HelloController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        public string Action(string name) =>
+            _greetingService.Greeting(name);
+
+    }
+}
