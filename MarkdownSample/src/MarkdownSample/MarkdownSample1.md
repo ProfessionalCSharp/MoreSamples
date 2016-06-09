d954b6ab80567af4c349bb320bdc6f49cbbd9d3b
Christian Nagel <christian@christiannagel.com>
2016-06-09 17:08:34 +0200

    add markdown sample using Markdig

---
+++ MarkdownSample/src/MarkdownSample/MarkdownSample1.md
@@ -0,0 +1,35 @@
+﻿# Heading 1
+
+This is a simple markdown sample
+
+## Heading 2
+
+Markdown, markdown
+
+[CN innovation](https://www.cninnovation.com "CN innovation")
+
+What is the best markdown library?
+
+I've never spent so much time writing on a new book as with **Professional C# 6 and .NET Core 1.0**
+
+What about an image?
+
+
+![An image](https://csharpdotchristiannageldotcom.files.wordpress.com/2016/06/professionalcsharp6.png "Professional C# 6")
+
+And source code as well:
+
+```csharp
+public string Foo(string s)
+{
+	return $"Hello, {s}";
+}
+```
+
+
+And a list:
+
+* One
+* Two
+* Three
+* Four
---
+++ MarkdownSample/src/MarkdownSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Markdig;
+using System;
+using System.IO;
+
+namespace MarkdownSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            try
+            {
+                string markdown = File.ReadAllText("../../src/MarkdownSample/MarkdownSample1.md");
+                string html = Markdown.ToHtml(markdown);
+                Console.WriteLine(html);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+        }
+    }
+}
