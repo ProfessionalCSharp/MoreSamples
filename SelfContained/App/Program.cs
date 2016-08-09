34734179e74ba23e4bc0e705dbe40e3ec4d916bc
Christian Nagel <christian@christiannagel.com>
2016-08-09 15:23:37 +0200

    add self-contained sample

---
+++ SelfContained/App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sample = new Sample();
+            sample.Foo();
+        }
+    }
+}
---
+++ SelfContained/Library/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Sample
+    {
+        public void Foo()
+        {
+            Console.WriteLine($"{nameof(ClassLibrary)}.{nameof(Sample)}.{nameof(Foo)}");
+        }
+    }
+}
