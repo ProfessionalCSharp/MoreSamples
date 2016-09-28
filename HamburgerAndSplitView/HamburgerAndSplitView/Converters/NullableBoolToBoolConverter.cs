faee50cecca7d79319c7496dc97df939c106e7f5
Christian Nagel <christian@christiannagel.com>
2016-09-28 10:29:27 +0200

    Hamburger and SplitView

---
+++ HamburgerAndSplitView/HamburgerAndSplitView/Converters/NullableBoolToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace HamburgerAndSplitView.Converters
+{
+    public class NullableBoolToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool? val = (bool?)value;
+            return val.HasValue ? val.Value : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => value;
+    }
+}
