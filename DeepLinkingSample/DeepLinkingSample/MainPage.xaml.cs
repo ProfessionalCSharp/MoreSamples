0617254406989d6491f5cb9e7f6c09ee7f18dbf2
Christian Nagel <christian@christiannagel.com>
2016-09-29 22:06:35 +0200

    UWP deep linking

---
+++ DeepLinkingSample/DeepLinkingSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DeepLinkingSample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DeepLinkingSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            ViewModel = new MainPageViewModel();
+            this.InitializeComponent();
+        }
+
+
+        MainPageViewModel ViewModel { get; }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel.Path = e.Parameter.ToString();
+        }
+
+    }
+}
---
+++ DeepLinkingSample/DeepLinkingSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+
+namespace DeepLinkingSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private string _path;
+
+        public string Path
+        {
+            get { return _path; }
+            set { SetProperty(ref _path, value); }
+        }
+
+    }
+}
