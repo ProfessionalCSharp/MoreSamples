3529c7ca098515d32ebe8573368c9cd27db215c2
Christian Nagel <christian@christiannagel.com>
2016-11-15 19:22:28 +0100

    deep linking sample

--- UWP/DeepLinkingSample/DeepLinkingSample/MainPage.xaml.cs
+++ UWP/DeepLinkingSample/DeepLinkingSample/MainPage.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using DeepLinkingSample.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -29,13 +17,11 @@ namespace DeepLinkingSample
             this.InitializeComponent();
         }
 
-
         MainPageViewModel ViewModel { get; }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             ViewModel.Path = e.Parameter.ToString();
         }
-
     }
 }
