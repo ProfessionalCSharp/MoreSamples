ea87136a3fe19fc209766d59d9f96f35c7e6d484
Christian Nagel <christian@christiannagel.com>
2017-01-20 14:27:45 +0100

    expression bodied members

---
+++ CSharp7/ExpressionBodiedMembers/ExpressionBodiedMembers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+// Expression Bodied Members Sample
+
+class Resource : IDisposable
+{
+    // C# 7.0 - Constructor
+    public Resource() => Console.WriteLine($"ctor {nameof(Resource)}");
+
+    // C# 7.0 - Destructor
+    ~Resource() => Console.WriteLine("Destructor");
+
+    // C# 6.0 - Method
+    public int UltimateAnswer() => 42;
+
+    // C# 6 - Get-accessor only Property
+    public int Y => 42;
+
+    public void Dispose() => Console.WriteLine("Disposing");
+
+    protected void Dispose(bool disposing)
+    {
+        if (disposing == true)
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    private int _x;
+
+    // C# 7.0 - Property Accessors
+    public int X
+    {
+        get => _x;
+        set => _x = value;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        using (var r = new Resource())
+        {
+            r.X = 3;
+            Console.WriteLine(r.X);
+        }
+    }
+}
