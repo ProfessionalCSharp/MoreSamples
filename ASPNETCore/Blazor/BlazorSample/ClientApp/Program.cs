645885a8e8debd60ceebefb979347d7b813e892c
Christian Nagel <christian@christiannagel.com>
2018-04-18 20:26:07 +0200

    initial blazor

---
+++ ASPNETCore/Blazor/BlazorSample/ClientApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ClientApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(services =>
+            {
+                
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+        }
+
+    }
+}
