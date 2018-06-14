82b1afadac1254e646564f6d4cc55a8fa0f44b74
Christian Nagel <christian@christiannagel.com>
2018-06-14 17:23:57 +0200

    nullable samples

---
+++ CSharp/NullableReferenceTypesSamples/NewAndGloryLib/NewAndGlory.cs
@@ -0,0 +1,16 @@
+﻿namespace NewAndGloryLib
+{
+    public class NewAndGlory
+    {
+        public string? GetANullString() => null;
+
+        public string GetAString() => "a string";
+
+        public string PassAString(string s) => s.ToUpper();
+    }
+
+    public interface INewInterface
+    {
+        string Bar();
+    }
+}
