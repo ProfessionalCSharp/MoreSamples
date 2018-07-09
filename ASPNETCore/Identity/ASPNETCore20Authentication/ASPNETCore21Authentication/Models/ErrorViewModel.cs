ca3fc4fa0c85fb1a97858340543d8e6b6b990f75
Christian Nagel <christian@christiannagel.com>
2018-07-09 13:35:40 +0200

    identity pages with ASP.NET Core 2.1

---
+++ ASPNETCore/Identity/ASPNETCore20Authentication/ASPNETCore21Authentication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCore21Authentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
