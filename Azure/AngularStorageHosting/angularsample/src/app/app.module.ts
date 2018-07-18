57223253208e0828d42c992eb3aeafed0f3b44d2
Christian Nagel <christian@christiannagel.com>
2018-07-18 09:51:47 +0200

    calling books api from angular

--- Azure/AngularStorageHosting/angularsample/src/app/app.module.ts
+++ Azure/AngularStorageHosting/angularsample/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Azure/AngularStorageHosting/angularsample/src/main.ts
+++ Azure/AngularStorageHosting/angularsample/src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+const providers = [
+  { provide: 'BASE_URL', useValue: 'https://angularsampleapp.azurewebsites.net/', deps: [] }
+];
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.log(err));
