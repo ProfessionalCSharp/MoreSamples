bf568e7279fd224500ef80473f328bc489aa6ce3
Christian Nagel <christian@christiannagel.com>
2018-07-24 14:34:31 +0200

    lost focus sample

---
+++ UWP/LostFocusSample/LostFocusSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LostFocusSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public string One { get; set; }
+        public string Two { get; set; }
+        public string Three { get; set; }
+
+        public async void ShowValues()
+        {
+            await new MessageDialog($"One: {One}, Two: {Two}, Three: {Three}").ShowAsync();
+        }
+
+        public void Refresh()
+        {
+            One = string.Empty;
+            Two = string.Empty;
+            Three = string.Empty;
+            Bindings.Update();
+        }
+    }
+}
