277806d595794e405a95818720c37a80d898c9fb
Christian Nagel <christian@christiannagel.com>
2018-11-01 13:28:58 +0100

    log to file

---
+++ DotnetCore/LoggingConfigurationSample/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Serilog;
+using System.IO;
+
+namespace LoggingConfigurationSample
+{
+    class Program
+    {
+        private static string s_url = "https://csharp.christiannagel.com";
+        private static TraceListener s_listener; // = new TextWriterTraceListener("c:/temp/logfile.txt");
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                s_url = args[0];
+            }
+
+
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            IConfiguration configuration = configurationBuilder.Build();
+            RegisterServices(configuration);
+            await RunSampleAsync();
+
+            // flush and close listeners and logs
+            s_listener.Flush();
+            s_listener.Dispose();
+            Log.CloseAndFlush();
+
+            AppServices.Dispose();
+          
+            Console.ReadLine();
+        }
+
+        static async Task RunSampleAsync()
+        {
+            var controller = AppServices.GetService<SampleController>();
+            await controller.NetworkRequestSampleAsync(s_url);
+        }
+
+        static void ConfigureSeriLogging()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("serilog.txt").CreateLogger();
+        }
+
+        static void ConfigureTraceSourceLogging()
+        {
+            var writer = File.CreateText("logfile.txt");
+
+            s_listener = new TextWriterTraceListener(writer.BaseStream);
+        }
+
+        static void RegisterServices(IConfiguration configuration)
+        {
+            ConfigureSeriLogging();
+            ConfigureTraceSourceLogging();
+
+            var services = new ServiceCollection();
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(configuration.GetSection("Logging"))
+                .AddTraceSource(new SourceSwitch("TraceSourceLog", SourceLevels.Verbose.ToString()), s_listener)
+                .AddSerilog()
+                .AddConsole();
+#if DEBUG
+                builder.AddDebug();
+#endif
+            });
+            services.AddHttpClient<SampleController>();
+            AppServices = services.BuildServiceProvider();
+           
+        }
+
+        public static ServiceProvider AppServices { get; private set; }
+    }
+}
---
+++ DotnetCore/LoggingConfigurationSample/SampleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LoggingConfigurationSample
+{
+    class SampleController
+    {
+        private readonly ILogger<SampleController> _logger;
+        private readonly HttpClient _httpClient;
+        public SampleController(HttpClient httpClient, ILogger<SampleController> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+            _logger.LogTrace("ILogger injected into {0}", nameof(SampleController));
+        }
+
+        public async Task NetworkRequestSampleAsync(string url)
+        {
+            try
+            {
+                _logger.LogInformation(LoggingEvents.Networking, "NetworkRequestSampleAsync started with url {0}", url);
+
+                string result = await _httpClient.GetStringAsync(url);
+                _logger.LogInformation(LoggingEvents.Networking, "NetworkRequestSampleAsync completed, received {0} characters", result.Length);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(LoggingEvents.Networking, ex, "Error in NetworkRequestSampleAsync, error message: {0}, HResult: {1}", ex.Message, ex.HResult);
+            }
+        }
+    }
+}
