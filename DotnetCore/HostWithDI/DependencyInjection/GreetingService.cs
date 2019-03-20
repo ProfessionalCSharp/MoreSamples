a5c49aa753aa7943b37228ac3aeec877882c0a87
Christian Nagel <christian@christiannagel.com>
2019-03-20 20:25:07 +0100

    use .net core 3 host

---
+++ DotnetCore/HostWithDI/DependencyInjection/GreetingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    public class GreetingService : IGreetingService
+    {
+        public string Greeting(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return $"Hello, {name}";
+        }
+    }
+}
---
+++ DotnetCore/HostWithDI/DependencyInjection/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace DependencyInjection
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices(ConfigureServices)
+                .Build();
+
+            UsingAContainer(host.Services);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreetingService, GreetingService>();
+            services.AddTransient<HelloController>();
+        }
+
+        private static void UsingAContainer(IServiceProvider container)
+        {
+            var controller = container.GetService<HelloController>();
+            string greeting = controller.Action("Stephanie");
+            Console.WriteLine(greeting);
+        }
+    }
+}
