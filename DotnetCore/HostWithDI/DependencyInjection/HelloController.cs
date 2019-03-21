21bd50d798599c44bdd48581dd7a415603a25571
Christian Nagel <christian@christiannagel.com>
2019-03-21 16:06:47 +0100

    using host with di

--- DotnetCore/HostWithDI/DependencyInjection/HelloController.cs
+++ DotnetCore/HostWithDI/DependencyInjection/HelloController.cs
@@ -1,12 +1,18 @@
-﻿namespace DependencyInjection
+﻿using Microsoft.Extensions.Logging;
+
+namespace DependencyInjection
 {
     public class HelloController
     {
         private readonly IGreetingService _greetingService;
-        public HelloController(IGreetingService greetingService) => 
-            _greetingService = greetingService;
+        private readonly ILogger _logger;
+        public HelloController(IGreetingService greetingService, ILogger<HelloController> logger) => 
+            (_greetingService, _logger) = (greetingService, logger);
 
-        public string Action(string name) =>
-            _greetingService.Greeting(name);
+        public string Action(string name)
+        {
+            _logger.LogTrace($"{nameof(HelloController)}.{nameof(Action)} invoked with {name}");
+            return _greetingService.Greeting(name);
+        }
     }
 }
--- DotnetCore/HostWithDI/DependencyInjection/Program.cs
+++ DotnetCore/HostWithDI/DependencyInjection/Program.cs
@@ -1,18 +1,42 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace DependencyInjection
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main(string[] args)
         {
-            using IHost host = Host.CreateDefaultBuilder()
-                .ConfigureServices(ConfigureServices)
-                .Build();
+            // Version 1
+            //using IHost host = Host.CreateDefaultBuilder()
+            //    .ConfigureServices(ConfigureServices)
+            //    .Build();
 
-            UsingAContainer(host.Services);
+            //UsingAContainer(host.Services);
+
+            // version 2
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
+                    config.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true)
+                        .AddEnvironmentVariables()
+                        .AddCommandLine(args))
+                .ConfigureLogging(logging =>
+                {
+                    // logging.AddConsole().AddFilter(level => level >= LogLevel.Error);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<GreetingHost>();
+                    services.AddTransient<IGreetingService, GreetingService>();
+                    services.AddTransient<HelloController>();
+                }).RunConsoleAsync(options => options.SuppressStatusMessages = false);
         }
 
         private static void ConfigureServices(IServiceCollection services)
