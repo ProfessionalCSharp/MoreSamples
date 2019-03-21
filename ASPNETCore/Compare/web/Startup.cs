e76266d3bb9717a40216afcc4fa4d5f01f13a41c
Christian Nagel <christian@christiannagel.com>
2019-03-21 18:25:53 +0100

    web project

--- ASPNETCore/Compare/web/Startup.cs
+++ ASPNETCore/Compare/web/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace simpleweb
+namespace web
 {
     public class Startup
     {
