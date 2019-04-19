23f2b477a3f633f87ccf3d3e1d60731642eeecb9
Christian Nagel <christian@christiannagel.com>
2019-04-19 21:50:46 +0200

    blazor preview4

--- ASPNETCore/Compare/blazor/Startup.cs
+++ ASPNETCore/Compare/blazor/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace blazor
@@ -9,7 +9,7 @@ namespace blazor
         {
         }
 
-        public void Configure(IBlazorApplicationBuilder app)
+        public void Configure(IComponentsApplicationBuilder app)
         {
             app.AddComponent<App>("app");
         }
