4c2eed4b89d9f03cd722479a473f78b1b52d2eba
Christian Nagel <christian@christiannagel.com>
2019-04-19 21:37:45 +0200

    grpc preview4

--- ASPNETCore/Compare/grpc/Startup.cs
+++ ASPNETCore/Compare/grpc/Startup.cs
@@ -26,9 +26,13 @@ namespace grpc
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapGrpcService<GreeterService>();
+                // Communication with gRPC endpoints must be made through a gRPC client.
+                // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
+                endpoints.MapGrpcService<GreeterService>();
             });
         }
     }
