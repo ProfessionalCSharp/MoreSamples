bb9adce09b15d81a32dc3fef56af2c8cf512693a
Christian Nagel <christian@christiannagel.com>
2019-05-05 21:19:48 -0700

    signalr

--- Azure/ClickTheButton/ClickToSignalRApp/ClickToSignalRFunction.cs
+++ Azure/ClickTheButton/ClickToSignalRApp/ClickToSignalRFunction.cs
@@ -6,17 +6,36 @@ using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace ClickToSignalRApp
 {
-    public static class ClickToSignalRFunction
+    public class ClickToSignalRFunction
     {
-        private static HttpClient client = new HttpClient();
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+                   [HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req,
+                   [SignalRConnectionInfo(HubName = "clickthebuttonhub")]SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
 
         [FunctionName("ClickToSignalRFunction")]
-        public static void Run([IoTHubTrigger("messages/events", Connection = "IOTButtonConnectionString")]EventData message, ILogger log)
+        public Task Run([IoTHubTrigger("messages/events", Connection = "IOTButtonConnectionString")]EventData message, 
+            [SignalR(HubName ="clickthebuttonhub")] IAsyncCollector<SignalRMessage> signalRMessages,
+            ILogger log)
         {
-            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+            string encoded = Encoding.UTF8.GetString(message.Body.Array);
+            log.LogInformation($"IoT Hub trigger processing message {encoded}");
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "buttonClicked",
+                    Arguments = new[] { encoded }
+                });
         }
     }
 }
---
+++ Azure/ClickTheButton/ClientApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("client - wait for service to be ready, please press return afterwards...");
+                Console.ReadLine();
+                var conn = new HubConnectionBuilder().WithUrl("http://localhost:7071/api/").Build();
+
+                Console.WriteLine("adding the handler");
+                conn.On("buttonClicked", (string s) =>
+                {
+                    Console.WriteLine($"Buttton clicked with message {Environment.NewLine}{s}");
+                });
+
+                await conn.StartAsync();
+                Console.WriteLine($"connection started, waiting for events {conn.State}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
