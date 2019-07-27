47b12a03567b76031f46f2987f047acc800ad09e
Christian Nagel <christian@christiannagel.com>
2019-07-27 13:44:15 +0200

    iot part 2

---
+++ Azure/ClickTheButton/Part2/ASPNETCoreSignalRService/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCoreSignalRService.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETCoreSignalRService
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSignalR(config =>
+            {
+                config.MapHub<IoTButtonHub>("/api/buttonhub");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("SignalR Sample");
+            });
+        }
+    }
+}
