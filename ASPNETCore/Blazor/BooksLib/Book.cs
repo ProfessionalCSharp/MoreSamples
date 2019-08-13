09f324870651d4411e31f03e0c0b7dee041d4fb3
Christian Nagel <christian@christiannagel.com>
2019-08-13 13:43:36 +0200

    server-side Blazor sample

---
+++ ASPNETCore/Blazor/BooksLib/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksLib
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Publisher { get; set; }
+    }
+}
---
+++ ASPNETCore/Blazor/BooksLib/BooksDataService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BooksLib
+{
+    public class BooksDataService : IBooksService
+    {
+        private readonly BooksContext _booksContext;
+        public BooksDataService(BooksContext booksContext)
+        {
+            _booksContext = booksContext;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await _booksContext.Books.ToListAsync();
+        }
+    }
+}
