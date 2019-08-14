bafb46af771445ea9c9eb6779885bc2f6c4bf0bf
Christian Nagel <christian@christiannagel.com>
2019-08-14 22:02:30 +0200

    blazor

--- ASPNETCore/Blazor/ServerSideAndClientSideBlazor/ClientSideBlazor/Client/Services/BooksApiClient.cs
+++ ASPNETCore/Blazor/ServerSideAndClientSideBlazor/ClientSideBlazor/Client/Services/BooksApiClient.cs
@@ -1,8 +1,6 @@
 ﻿using BooksLib;
 using Microsoft.AspNetCore.Components;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
