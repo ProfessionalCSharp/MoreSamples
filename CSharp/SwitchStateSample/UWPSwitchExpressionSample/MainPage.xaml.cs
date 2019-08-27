5da2c6e1b716b398eb6a97188244f28feaf5d0a7
Christian Nagel <christian@christiannagel.com>
2019-08-27 09:49:55 +0200

    switch with uwp

---
+++ CSharp/SwitchStateSample/UWPSwitchExpressionSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPSwitchExpressionSample
+{
+    public sealed partial class MainPage : Page
+    {
+       
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LightState = LightState.Red;
+            previousState = LightState.Yellow;
+
+            var switcher = new TrafficLightStateSwitcher();
+
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += (sender, e) =>
+            {
+                (LightState, previousState) = switcher.GetLight(LightState, previousState);
+            };
+            timer.Start();
+
+        }
+
+        private LightState previousState;
+
+        public LightState LightState
+        {
+            get => (LightState)GetValue(LightStateProperty);
+            set => SetValue(LightStateProperty, value);
+        }
+
+        public static readonly DependencyProperty LightStateProperty =
+            DependencyProperty.Register("LightState", typeof(LightState), typeof(MainPage), new PropertyMetadata(null));
+
+    }
+}
---
+++ CSharp/SwitchStateSample/UWPSwitchExpressionSample/TrafficLightStateSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UWPSwitchExpressionSample
+{
+    public class TrafficLightStateSwitcher
+    {
+        public (LightState Current, LightState Previous) GetLight(LightState currentLight, LightState previousLight)
+        => (currentLight, previousLight) switch
+        {
+            (LightState.Red, _) => (LightState.Yellow, currentLight),
+            (LightState.Yellow, LightState.Red) => (LightState.Green, currentLight),
+            (LightState.Green, _) => (LightState.Yellow, currentLight),
+            (LightState.Yellow, LightState.Green) => (LightState.Red, currentLight),
+            _ => throw new InvalidOperationException()
+        };
+    }
+}
