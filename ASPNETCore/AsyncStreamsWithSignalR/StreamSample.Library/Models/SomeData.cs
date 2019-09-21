6520af1cd8c7860a16387e414b9692ff43adbc02
Christian Nagel <christian@christiannagel.com>
2019-09-21 10:19:16 +0200

    async streams sample

---
+++ ASPNETCore/AsyncStreamsWithSignalR/StreamSample.Library/Models/SomeData.cs
@@ -0,0 +1,9 @@
+﻿namespace StreamSample.Models
+{
+    public struct SomeData
+    {
+        public int Value { get; set; }
+
+        public override string ToString() => Value.ToString();
+    }
+}
