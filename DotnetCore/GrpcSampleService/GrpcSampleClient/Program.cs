d57645c7740647c3f270a7498b1b79e12ec9cfc0
Christian Nagel <christian@christiannagel.com>
2019-10-01 22:28:29 +0200

    initial grpc sample

---
+++ DotnetCore/GrpcSampleService/GrpcSampleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcSampleService;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcSampleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Client - wait for the service to start");
+            Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "Katharina" });
+            Console.WriteLine(response.Message);
+
+
+        }
+    }
+}
