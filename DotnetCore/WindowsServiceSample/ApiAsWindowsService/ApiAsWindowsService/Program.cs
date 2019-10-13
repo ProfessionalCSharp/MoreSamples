f83dcb46752b4b971626265fcf755f07e28c5e3e
Christian Nagel <christian@christiannagel.com>
2019-10-13 19:40:51 +0200

    api as windows service

--- DotnetCore/WindowsServiceSample/ApiAsWindowsService/ApiAsWindowsService/Program.cs
+++ DotnetCore/WindowsServiceSample/ApiAsWindowsService/ApiAsWindowsService/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.EventLog;
 
 namespace ApiAsWindowsService
 {
@@ -12,13 +14,21 @@ namespace ApiAsWindowsService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.Configure<EventLogSettings>(config =>
+                    {
+                        config.LogName = "Sample API Service";
+                        config.SourceName = "Sample API Service Source";
+                    });
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 })
                 .ConfigureWebHost(config => 
                 {
-                    config.UseUrls("http://localhost:5050"); 
+                    config.UseUrls("http://*:5050"); 
                 }).UseWindowsService();
     }
 }
