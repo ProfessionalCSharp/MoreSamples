bf7c12de90fa9f6a60ed6e7f1ff56ad1f734759d
Christian Nagel <christian@christiannagel.com>
2019-10-13 18:11:54 +0200

    cleanup windows service sample

--- DotnetCore/WindowsServiceSample/WindowsServiceSample/Program.cs
+++ DotnetCore/WindowsServiceSample/WindowsServiceSample/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.EventLog;
 
 namespace WindowsServiceSample
@@ -13,13 +15,15 @@ namespace WindowsServiceSample
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(
+                    options => options.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Information))
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>()
                         .Configure<EventLogSettings>(config =>
                         {
-                            config.LogName = "My Log";
-                            config.SourceName = "My Source";
+                            config.LogName = "Sample Service";
+                            config.SourceName = "Sample Service Source";
                         });
                 }).UseWindowsService();
     }
--- DotnetCore/WindowsServiceSample/WindowsServiceSample/Worker.cs
+++ DotnetCore/WindowsServiceSample/WindowsServiceSample/Worker.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WindowsServiceSample
 {
