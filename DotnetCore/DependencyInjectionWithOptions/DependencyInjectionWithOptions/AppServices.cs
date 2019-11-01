0b3f839153240f0a7edc5bbbba29a4652d1b28f3
Christian Nagel <christian@christiannagel.com>
2019-11-01 20:56:31 +0100

    add host to DI options

---
+++ DotnetCore/DependencyInjectionWithOptions/DependencyInjectionWithOptions/AppServices.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionWithOptions
+{
+    public class AppServices
+    {
+        private AppServices()
+        {
+            Container = GetServices();
+        }
+
+        private static AppServices? _instance;
+        public static AppServices Instance => GetAppServices();
+
+        private static object _sync = new object();
+        private static AppServices GetAppServices()
+        {
+            lock (_sync)
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppServices();
+                }
+                return _instance;
+            }
+        }
+
+        public IServiceProvider Container { get; }
+
+        public IServiceProvider GetServices()
+        {
+            var services = new ServiceCollection();
+            services.AddOptions();
+            services.AddTransient<HelloController>();
+
+            services.AddGreetingService(options =>
+            {
+                options.From = "Christian";
+            });
+            return services.BuildServiceProvider();
+        }
+    }
+}
