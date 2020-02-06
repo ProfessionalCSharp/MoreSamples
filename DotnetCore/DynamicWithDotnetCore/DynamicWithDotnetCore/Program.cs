1b0f93954ed195acbe2ddc111204989ac5bd005b
Christian Nagel <christian@christiannagel.com>
2020-02-06 17:29:58 +0100

    dynamic

---
+++ DotnetCore/DynamicWithDotnetCore/DynamicWithDotnetCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace DynamicWithDotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || string.IsNullOrEmpty(args[0]))
+            {
+                Console.WriteLine($"add a path to the lib");
+                return;
+            }
+            var assembly = Assembly.LoadFile(args[0]);
+            var type = assembly.GetType("JustALib.Calculator");
+            object calc = Activator.CreateInstance(type);
+
+            UseReflection(calc);
+            UseDynamic(calc);
+        }
+
+        private static void UseReflection(object calc)
+        {
+            var method = calc.GetType().GetMethod("Add");
+            var result = method.Invoke(calc, new object[] { 34, 8 });
+            Console.WriteLine(result);
+        }
+
+        private static void UseDynamic(dynamic calc)
+        {
+            var result = calc.Add(34, 8);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ DotnetCore/DynamicWithDotnetCore/JustALib/Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace JustALib
+{
+    public class Calculator
+    {
+        public int Add(int x, int y) => x + y;
+    }
+}
