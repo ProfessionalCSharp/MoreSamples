a4c32c867cb036794dbba0b9dc07866127acd67f
Christian Nagel <christian@christiannagel.com>
2020-03-10 08:25:58 +0100

    package updates

--- DotnetCore/GrpcBooksSample/GrpcSampleService/GrpcSampleClient/Program.cs
+++ DotnetCore/GrpcBooksSample/GrpcSampleService/GrpcSampleClient/Program.cs
@@ -12,7 +12,7 @@ namespace GrpcSampleClient
         {
             Console.WriteLine("client - press return to call the server");
             Console.ReadLine();
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");            
             var client = new Books.BooksClient(channel);
             var reply = await client.GetBookAsync(new BookRequest { Id = 42 }, new CallOptions() { });
             Console.WriteLine($"{reply.Title} {reply.Publisher}");
