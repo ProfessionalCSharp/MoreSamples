dcd0b34babc9b3c69b3c6054a7c6230902e721d0
Christian Nagel <christian@christiannagel.com>
2020-09-29 13:13:30 +0200

    update ef core cosmos sample to .NET Core 3.1

--- Azure/CosmosDBWithEFCoreAndRelations/CosmosDBWithEFCore/BooksContext.cs
+++ Azure/CosmosDBWithEFCoreAndRelations/CosmosDBWithEFCore/BooksContext.cs
@@ -11,7 +11,7 @@ namespace CosmosDBWithEFCore
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasDefaultContainerName("BooksContainer");
+            modelBuilder.HasDefaultContainer("BooksContainer");
             // entity types can be explicitly mapped to containers
             // modelBuilder.Entity<Book>().ToContainer("BooksContainer");
             modelBuilder.Entity<Book>().OwnsOne(b => b.LeadAuthor);
