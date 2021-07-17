5a7791781fd0e92d505dfcc17f6c2278025b440f
Christian Nagel <christian@christiannagel.com>
2021-07-17 12:18:57 +0200

    initial azure app config sample

---
+++ Azure/AppConfig/02-ConfigSample/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ConfigSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(IOptions<IndexAppSettings> options, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Setting1 = options.Value.Config1 ?? "no value configured";
+        }
+
+        public string Setting1 { get; init; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Azure/AppConfig/02-ConfigSample/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var settings = config.Build();
+                    var connectionString = settings["AzureAppConfigurationConnection"];
+                    config.AddAzureAppConfiguration(connectionString);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
