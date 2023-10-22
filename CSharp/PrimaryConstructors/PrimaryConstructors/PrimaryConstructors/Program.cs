17df310e2a086efab2f7115bea42c2c3b691a693
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2023-10-22 04:44:12 +0400

    PROVISIONS: Primary constructors

---
+++ CSharp/PrimaryConstructors/PrimaryConstructors/PrimaryConstructors/Program.cs
@@ -0,0 +1,17 @@
+﻿public class NamedItem(string name)
+{
+    public string Name => name;
+}
+
+// name isn't captured in Widget.
+// width, height, and depth are captured as private fields
+public class Widget(string name, int width, int height, int depth) : NamedItem(name)
+{
+    public Widget() : this("N/A", 1, 1, 1) { } // unnamed unit cube
+
+    public int WidthInCM => width;
+    public int HeightInCM => height;
+    public int DepthInCM => depth;
+
+    public int Volume => width * height * depth;
+}
